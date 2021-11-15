7da08596dc4ac36a8f643cd682a7232623dd17a0
beardad1975 <beardad1975@nmes.tyc.edu.tw>
2021-11-15 22:04:44 +0800

    bumped version 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="pie4t",
-    version="0.0.7",
+    version="0.0.8",
     author="Wen-Hung, Chang 張文宏",
     author_email="beardad1975@nmes.tyc.edu.tw",
     description="Physics Impulse Engine wrapper for Teenagers",
