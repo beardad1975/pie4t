a8baadf92fd28752430fac7ec3ea3507f07caabd
beardad1975 <beardad1975@nmes.tyc.edu.tw>
2023-01-31 17:47:43 +0800

    bumped version 0.1.0 (update dep library)

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ import setuptools
 
 setuptools.setup(
     name="pie4t",
-    version="0.0.9",
+    version="0.1.0",
     author="Wen-Hung, Chang 張文宏",
     author_email="beardad1975@nmes.tyc.edu.tw",
-    description="Physics Impulse Engine wrapper for Teenagers",
-    long_description="Physics Impulse Engine wrapper for Teenagers",
+    description="Physics Impulse Engine wrapper library for Teenagers",
+    long_description="Physics Impulse Engine wrapper library for Teenagers",
     long_description_content_type="text/markdown",
     url="https://github.com/beardad1975/pie4t",
     #packages=setuptools.find_packages(),
     platforms=["Windows"],
-    python_requires=">=3.5",
+    python_requires=">=3.8",
     packages=['pie4t','物理模組'],
-    install_requires = ['arcade~=2.6.0', 'pymunk~=6.2.0'],
+    install_requires = ['arcade~=2.6.0', 'pymunk~=6.4.0'],
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
